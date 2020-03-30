882329483befa8681b89dc8e1a14119ebc3ef09c
Matthew Askes <matthewaskes@gmail.com>
2020-03-30 13:51:01 +1300

    configure initial java project

---
+++ src/main.java
@@ -0,0 +1,14 @@
+/**
+ * @author Matthew Askes
+ */
+public class main {
+    
+    public main(){
+        System.out.println("Hello world 2");
+        
+    }
+    
+    public static void main(String[] args) {
+        new main();
+    }
+}
